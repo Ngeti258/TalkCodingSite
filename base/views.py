bff97a32a04f8399ea087b736408c3d1803c7c82
ngeti258 <gabrielngeti4@gmail.com>
2024-02-22 00:21:16 +0300

    added custom userCreation from instead of default user model

--- base/views.py
+++ base/views.py
@@ -3,11 +3,10 @@ from django.contrib import messages
 from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room ,Topic ,Message
-from .form import RoomForm,UserForm
+
+from .models import Room ,Topic ,Message,User
+from .form import RoomForm,UserForm,myUserCreationForm
 
 
 
@@ -22,15 +21,15 @@ def loginPage(request):
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username = username)
+            user = User.objects.get(email = email)
         except:
             messages.error(request,'User does not exist')
 
-        user = authenticate(request, username = username, password = password)
+        user = authenticate(request, email = email, password = password)
 
         if user is not None:
             login(request,user)
@@ -46,10 +45,10 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = myUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = myUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -68,7 +67,7 @@ def home(request):
         Q(name__icontains=q)|
         Q(description__icontains=q))
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
     context = {'rooms':rooms,'topics':topics,'room_count':room_count,'room_messages':room_messages}
@@ -167,8 +166,17 @@ def updateUser(request):
     user = request.user
     form = UserForm(instance=user)
     if request.method == "POST":
-        form = UserForm(request.POST,instance=user)
+        form = UserForm(request.POST,request.FILES,instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile',pk=user.id)
     return render(request,'base/update-user.html',{'form':form})
+
+def topicsPage(request):
+    q=request.GET.get('q') if request.GET.get('q') != None else ""
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request,'base/topics.html',{'topics': topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all
+    return render(request,'base/activity.html',{'room_messages':room_messages})
