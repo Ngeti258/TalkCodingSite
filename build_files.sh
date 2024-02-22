cc5661140bc8e11320dc2adc499f1713333759c5
ngeti258 <gabrielngeti4@gmail.com>
2024-02-23 01:39:13 +0300

    new

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@
 # Install Python dependencies
 
 # Collect static files
-python3.9 -m  pip install -r requirements.txt
+python -m  pip install -r requirements.txt
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput --clear
