f98f8b4f4b3f8cd073795b8e3466c3849b2aff0b
ngeti258 <gabrielngeti4@gmail.com>
2024-02-22 17:58:39 +0300

    updated settings to accept all hosts

--- studybud/settings.py
+++ studybud/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*1x)(2by$y+war$p+84!+vkd3uh6d@wc0v!-#qd*8zh@$ggjoc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
