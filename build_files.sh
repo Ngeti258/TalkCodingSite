016bff26000e6482e8cb2480f25b28954bbc23a2
ngeti258 <gabrielngeti4@gmail.com>
2024-02-23 11:16:34 +0300

    switching deploy to EC2

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Python dependencies
-
-# Collect static files
-python3.9 -m  pip install -r requirements.txt
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic --noinput --clear
