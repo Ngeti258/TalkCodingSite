aa5d184a7f3704e3d9f6a8bc5a9c67c44ae7371d
ngeti258 <gabrielngeti4@gmail.com>
2024-03-04 11:11:22 +0300

    turned off debug to make ready for production

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*1x)(2by$y+war$p+84!+vkd3uh6d@wc0v!-#qd*8zh@$ggjoc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
